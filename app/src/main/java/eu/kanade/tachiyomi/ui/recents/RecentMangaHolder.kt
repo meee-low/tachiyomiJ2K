84427b3ac7deca9d4f58073a6aa39c14c60741a5
Jays2Kings <jays@outlook.com>
2022-06-29 18:12:28 -0400

    remove wildcard in RecentMangaHolder

--- app/src/main/java/eu/kanade/tachiyomi/ui/recents/RecentMangaHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/recents/RecentMangaHolder.kt
@@ -7,7 +7,7 @@ import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
-import eu.kanade.tachiyomi.R.*
+import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.download.model.Download
 import eu.kanade.tachiyomi.data.image.coil.loadManga
 import eu.kanade.tachiyomi.databinding.RecentMangaItemBinding
@@ -57,22 +57,22 @@ class RecentMangaHolder(
             it.updateLayoutParams<ConstraintLayout.LayoutParams> {
                 if (isSmallUpdates) {
                     if (it == binding.title) topMargin = 5.dpToPx
-                    endToStart = id.button_layout
+                    endToStart = R.id.button_layout
                     endToEnd = -1
                 } else {
                     if (it == binding.title) topMargin = 2.dpToPx
                     endToStart = -1
-                    endToEnd = id.front_view
+                    endToEnd = R.id.front_view
                 }
             }
         }
         binding.buttonLayout.updateLayoutParams<ConstraintLayout.LayoutParams> {
             if (isSmallUpdates) {
                 topToBottom = -1
-                topToTop = id.front_view
+                topToTop = R.id.front_view
             } else {
                 topToTop = -1
-                topToBottom = id.subtitle
+                topToBottom = R.id.subtitle
             }
         }
         val freeformCovers = !isSmallUpdates && !adapter.uniformCovers
@@ -93,7 +93,7 @@ class RecentMangaHolder(
         binding.removeHistory.isVisible = item.mch.history.id != null && showRemoveHistory
         val chapterName = if (item.mch.manga.hideChapterTitle(adapter.preferences)) {
             val number = adapter.decimalFormat.format(item.chapter.chapter_number.toDouble())
-            itemView.context.getString(string.chapter_, number)
+            itemView.context.getString(R.string.chapter_, number)
         } else item.chapter.name
         binding.title.apply {
             text = if (!showTitleFirst) {
@@ -113,40 +113,40 @@ class RecentMangaHolder(
         }
         if (binding.frontView.translationX == 0f) {
             binding.read.setImageResource(
-                if (item.read) drawable.ic_eye_off_24dp else drawable.ic_eye_24dp,
+                if (item.read) R.drawable.ic_eye_off_24dp else R.drawable.ic_eye_24dp,
             )
         }
         val notValidNum = item.mch.chapter.chapter_number <= 0
         binding.body.isVisible = !isSmallUpdates
         val context = itemView.context
         binding.body.text = when {
-            item.mch.chapter.id == null -> context.timeSpanFromNow(string.added_, item.mch.manga.date_added)
+            item.mch.chapter.id == null -> context.timeSpanFromNow(R.string.added_, item.mch.manga.date_added)
             isSmallUpdates -> ""
             item.mch.history.id == null -> {
                 if (adapter.viewType == RecentsPresenter.VIEW_TYPE_ONLY_UPDATES) {
                     if (adapter.sortByFetched) {
-                        context.timeSpanFromNow(string.fetched_, item.chapter.date_fetch)
+                        context.timeSpanFromNow(R.string.fetched_, item.chapter.date_fetch)
                     } else {
-                        context.timeSpanFromNow(string.updated_, item.chapter.date_upload)
+                        context.timeSpanFromNow(R.string.updated_, item.chapter.date_upload)
                     }
                 } else {
-                    context.timeSpanFromNow(string.fetched_, item.chapter.date_fetch) + "\n" +
-                        context.timeSpanFromNow(string.updated_, item.chapter.date_upload)
+                    context.timeSpanFromNow(R.string.fetched_, item.chapter.date_fetch) + "\n" +
+                        context.timeSpanFromNow(R.string.updated_, item.chapter.date_upload)
                 }
-                context.timeSpanFromNow(string.updated_, item.chapter.date_upload)
+                context.timeSpanFromNow(R.string.updated_, item.chapter.date_upload)
             }
-            item.chapter.id != item.mch.chapter.id -> context.timeSpanFromNow(string.read_, item.mch.history.last_read) +
+            item.chapter.id != item.mch.chapter.id -> context.timeSpanFromNow(R.string.read_, item.mch.history.last_read) +
                 "\n" + binding.body.context.getString(
-                if (notValidNum) string.last_read_ else string.last_read_chapter_,
+                if (notValidNum) R.string.last_read_ else R.string.last_read_chapter_,
                 if (notValidNum) item.mch.chapter.name else adapter.decimalFormat.format(item.mch.chapter.chapter_number),
             )
-            item.chapter.pages_left > 0 && !item.chapter.read -> context.timeSpanFromNow(string.read_, item.mch.history.last_read) +
+            item.chapter.pages_left > 0 && !item.chapter.read -> context.timeSpanFromNow(R.string.read_, item.mch.history.last_read) +
                 "\n" + itemView.resources.getQuantityString(
-                plurals.pages_left,
+                R.plurals.pages_left,
                 item.chapter.pages_left,
                 item.chapter.pages_left,
             )
-            else -> context.timeSpanFromNow(string.read_, item.mch.history.last_read)
+            else -> context.timeSpanFromNow(R.string.read_, item.mch.history.last_read)
         }
         if ((context as? Activity)?.isDestroyed != true) {
             binding.coverThumbnail.loadManga(item.mch.manga)
